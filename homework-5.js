df1e82156b33bb5f8e891ec472c83fb2e10565d5
karik0681 <ibroibro@gmail.com>
2026-01-17 23:19:32 +0100

    Corrected commit message: Добавлены функции и переменные для работы с пользователем

--- homework-5.js
+++ homework-5.js
@@ -1,18 +1,18 @@
-// Показывает погоду в указанном городе.
-function showWeather(city, temperature) {
-    console.log(`Погода в ${city} составляет ${temperature}°C`);
+// Показывает температуру в указанном городе.
+function showTemperature(city, temperature) {
+    console.log(`Температура в ${city} составляет ${temperature}°C`);
 }
-showWeather('Москва', 20);
+showTemperature('Москва', 20);
 
 // Скорость света в вакууме
 const speedOfLight = 299792458;
 function checkSpeedOfLight(speed) {
     if (speed === speedOfLight) {
-        console.log('Это правильная скорость света в вакууме.');
+        console.log('Скорость света.');
     } else if (speed < speedOfLight) {
-        console.log('Это слишком низкая скорость света в вакууме.');
+        console.log('Субсветовая скорость.');
     } else {
-        console.log('Это неправильная скорость света в вакууме.');
+        console.log('Сверхсветовая скорость.');
     }
 }
 checkSpeedOfLight(300000000);
@@ -21,16 +21,29 @@ checkSpeedOfLight(250000000);
 
 // Бюджет на покупку техники
 const product  = 'ноутбук';
-const deviceCost = 1200;
+const devicePrice = 1200;
 function canPurchaseDevice(budget) {
 
-    if (budget >= deviceCost) {
+    if (budget >= devicePrice) {
         console.log(`Ваш ${product} куплен.  Спасибо за покупку!`);
-    } else {
-        const difference = deviceCost - budget;
-        console.log(`Не хватает ${difference} долларов.`);
+    } else{
+        const difference = devicePrice - budget;
+        console.log(`Вам не хватает ${difference} долларов.`);
 
     }
 }
 canPurchaseDevice(800);
 canPurchaseDevice(1200);
+
+
+// Длина строки
+const result = length('Hello, world!');
+console.log(result); // Выведет: 13
+
+
+
+
+// Переменные с информацией о пользователе
+const userName = 'Алексей';
+const userAge = 30;
+const userCity = 'Москва';
