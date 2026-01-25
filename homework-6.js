016a6524533068e82f0b4a643642cc9456f15c07
karik0681 <ibroibro@gmail.com>
2026-01-25 17:12:25 +0100

    homework-6.js

---
+++ homework-6.js
@@ -0,0 +1,29 @@
+// Создайте объект, содержащий информацию о себе: имя, фамилия, возраст, город, страна, email, род деятельности и компания.
+// Выведите этот объект в консоль.
+
+const myInfo = {
+    firstName: 'Kairat',
+    lastName: 'Ivanov',
+    age: '30',
+    city: 'Almaty',
+    country: 'Kazakhstan',
+    email: 'kairat.ivanov@example.com',
+    work : 'Software Engineer',
+    company: 'Tech Solutions'
+};
+
+console.log(myInfo);
+
+Содайте объект об автомобиле: марка, модель, год выпуска, цвет, пробег, владелец myInfo (объект созданный в предыдущем задании).
+
+const car = {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2020,
+    transmission: 'Automatic',
+    color: 'White',
+    mileage: 15000,
+    owner: myInfo
+};
+
+console.log(car);
