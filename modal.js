d235bc183224aeb869b0887d2eb445fd7009e874
karik0681 <ibroibro@gmail.com>
2026-04-27 23:46:37 +0200

    Modal — одна функция handleClose, closeBtn в конструкторе

--- modal.js
+++ modal.js
@@ -1,28 +1,27 @@
 class Modal {
-    #handleOverlayClick;
-    #handleCloseClick;
+    #handleClose;
+
     constructor(modal, openBtn = null, shouldCloseOnOverlay = false) {
         this.modal = document.getElementById(modal);
         this.overlay = document.getElementById('modal-overlay');
+        this.closeBtn = this.modal.querySelector('.close-btn');
         this.shouldCloseOnOverlay = shouldCloseOnOverlay;
-        this.#handleOverlayClick = () => this.close();
-        this.#handleCloseClick = () => this.close();
+        this.#handleClose = () => this.close();
         if (openBtn) this.#initOpen(openBtn);
-
     }
 
     open() {
         this.modal.classList.add('modal-showed');
         this.overlay.classList.add('overlay-showed');
         if (this.shouldCloseOnOverlay) {
-            this.overlay.addEventListener('click', this.#handleOverlayClick);
+            this.overlay.addEventListener('click', this.#handleClose);
         }
         this.#initClose();
     }
 
     close() {
-        this.overlay.removeEventListener('click', this.#handleOverlayClick);
-        this.modal.querySelector('.close-btn').removeEventListener('click', this.#handleCloseClick);
+        this.overlay.removeEventListener('click', this.#handleClose);
+        this.closeBtn.removeEventListener('click', this.#handleClose);
         this.modal.classList.remove('modal-showed');
         this.overlay.classList.remove('overlay-showed');
     }
@@ -37,8 +36,7 @@ class Modal {
     }
 
     #initClose() {
-        const closeBtn = this.modal.querySelector('.close-btn');
-        closeBtn.addEventListener('click', this.#handleCloseClick);
+        this.closeBtn.addEventListener('click', this.#handleClose);
     }
 }
 
